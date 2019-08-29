b8b466cc1ce35cfa41e676113e1ee48597e95e9c
Mina Samir <MenaSamerCap@gmail.com>
2019-08-29 18:57:09 +0200

    fored portrait mode only for mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mhatb/screens/HomeScreen.dart';
 import 'package:mhatb/screens/SplashScreen.dart';
 
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    /// to force protrait mode only for app ...
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       title: APP_NAME,
       theme: ThemeData(
